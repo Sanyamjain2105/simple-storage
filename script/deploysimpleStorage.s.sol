d2ab9c00a645a13a055c4501f5058c9e469692df
Sanyam <sanyamjain353@gmail.com>
2025-09-15 00:47:17 +0530

    learning more

--- script/deploysimpleStorage.s.sol
+++ script/deploysimpleStorage.s.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
-import {SimpleStorage} from "../src/SimpleStorage.sol";
-
+import {SimpleStorage} from "../src/simpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
